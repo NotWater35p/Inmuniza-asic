b35494c8900e39fd3cb4c8a2be53a180216049a9
NotWater35p <danyherazo548@gmail.com>
2026-06-17 03:37:05 -0400

    reparacion del sispai

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -672,13 +672,13 @@
                     </ul>
                 </li>
                 <li>
-                <li>
+                {{-- <li>
                     <a href="{{ route('sispai.index') }}"
                         class="sidebar-link flex items-center gap-3 px-2 py-1.5 text-body rounded-base hover:bg-blue-50 hover:text-blue-600 w-full {{ str_starts_with($rutaActual, 'sispai') ? 'active' : '' }}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" /><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /></svg>
                         <span>Reportes</span>
                     </a>
-                </li>
+                </li> --}}
 
                 @endif {{-- fin @else admin --}}
 
